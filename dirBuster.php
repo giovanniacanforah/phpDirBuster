981f791bb4ff363b1a3c0d4436da1b88beb7d779
giovanniacanforah <schizzapple@gmail.com>
2019-06-03 19:48:54 +0200

    update file
    Rimozione di un if inutile, sostituzione di un normale scandir con array_diff

--- dirBuster.php
+++ dirBuster.php
@@ -1,14 +1,13 @@
+<?php 
 class dirBuster {
     public function Buster($dir = __DIR__.'/') {
-        foreach (scandir($dir) as $element) {
+        foreach (array_diff(scandir($dir), ['.','..'] as $element) {
             $finaldir = $dir.$element;
-            if(!in_array($element, ['.', '..'])){
-                if (is_dir($finaldir)) {
-                    echo $finaldir.PHP_EOL;
-                    $this->Buster($finaldir.'/');
-                }else{
-                    echo $finaldir.PHP_EOL;
-                }
+            if (is_dir($finaldir)) {
+                echo $finaldir.PHP_EOL;
+                $this->Buster($finaldir.'/');
+            }else{
+                echo $finaldir.PHP_EOL;
             }
         }
     }
